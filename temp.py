7d85a9b01fd2d8940040018a322ad8499a01b643
Mohammed Shoaib <shoaib91483@gmail.com>
2026-09-01 12:34:07 +0530

    Delete temp.py

--- temp.py
+++
@@ -1,7 +0,0 @@
-from src.models.explain import explain_customer
-
-for cid in ["16029", "12346", "13093"]:
-    result = explain_customer(cid)
-    print(f"\nCustomer {cid}:")
-    for r in result["top_reasons"]:
-        print(f"  {r['feature']}: value={r['value']}, shap={r['shap_value']}, {r['direction']}")
